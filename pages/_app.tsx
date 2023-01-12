b3a9d3a49128bbbd276c8b8a1236c6bc0ca73091
Corliansa Kusumah <corliansa@gmail.com>
2023-01-12 16:26:39 +0100

    add plausible

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,20 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Footer from "../components/Footer";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }: AppProps) {
-	return (
-		<>
-			<Component {...pageProps} />
-			<Footer />
-		</>
-	);
+  return (
+    <>
+      <Script
+        defer
+        data-domain="corliansa.xyz"
+        src="https://plausible.dlx.pw/js/plausible.js"
+      />
+      <Component {...pageProps} />
+      <Footer />
+    </>
+  );
 }
 
 export default MyApp;
