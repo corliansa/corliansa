46d84d127bede059912d416fc2576a0e5e1e4213
Corliansa Kusumah <corliansa@gmail.com>
2023-01-26 20:23:01 +0100

    added plausible

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,20 @@
 import { type AppType } from "next/app";
-
+import Script from "next/script";
 import { api } from "../utils/api";
 
 import "../styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Script
+        defer
+        data-domain="corliansa.xyz"
+        src="https://plausible.dlx.pw/js/script.outbound-links.file-downloads.tagged-events.js"
+      />
+      <Component {...pageProps} />
+    </>
+  );
 };
 
 export default api.withTRPC(MyApp);
