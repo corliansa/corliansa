a9e3bfbc4dc7ac4f9ead5752dc6cd36dcc3dc098
Corliansa Kusumah <corliansa@gmail.com>
2024-09-03 02:19:42 +0700

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,7 +12,7 @@ const Home: NextPage = () => {
           Corliansa Kusumah
         </h1>
         <p className="my-6 text-lg leading-8 text-gray-400 sm:text-center">
-          I&apos;m Corliansa, a graduate of <a href="https://tu.berlin/" target="_blank">Technische Universität Berlin</a> in Computer Science. I&apos;m passionate about using technology to solve real-world problems, particularly at the intersection of finance and technology. I am currently working at <a href="https://coinbase.com/" target="_blank">Coinbase</a>
+          I&apos;m Corliansa, a graduate of <a href="https://tu.berlin/" target="_blank" rel="noreferrer">Technische Universität Berlin</a> in Computer Science. I&apos;m passionate about using technology to solve real-world problems, particularly at the intersection of finance and technology. I am currently working at <a href="https://coinbase.com/" target="_blank" rel="noreferrer">Coinbase</a>
         </p>
         <div className="flex gap-2 sm:justify-center">
           <Link href="https://linkedin.com/in/corliansa/" target="_blank">
